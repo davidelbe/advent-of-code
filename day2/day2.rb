27bc88d47a0bfc4641135bde1b2300194d796bad
David Elbe <david@davidelbe.local>
2017-12-02 14:11:23 +0100

    Add day 2 - part 2

--- day2/day2.rb
+++ day2/day2.rb
@@ -8,8 +8,25 @@ class CorruptionChecksum
     digits = line.split(" ").collect(&:to_i)
     digits.max - digits.min
   end
+
+  def division_checksum(input)
+    lines = input.split("\n")
+    lines.collect { |l| line_division_checksum(l) }.inject(0, :+)
+  end
+
+  def line_division_checksum(line)
+    digits = line.split(" ").collect(&:to_i)
+    digits.each_with_index do |digit, index|
+      friends = digits.dup
+      friends.delete_at(index)
+      divided = friends.collect{ |f| digit / f if digit % f == 0 }.compact
+      next unless divided.any?
+      return divided.first
+    end
+  end
 end
 
 input = File.read("day2_input.txt")
 checker = CorruptionChecksum.new
 puts checker.checksum(input)
+puts checker.division_checksum(input)
--- day2/day2_test.rb
+++ day2/day2_test.rb
@@ -2,15 +2,27 @@ require "minitest/autorun"
 require_relative "day2.rb"
 
 class CorruptionChecksumTest < Minitest::Test
+  def checker
+    checker = CorruptionChecksum.new
+  end
+
   def test_checksum_single_line
     input = "5 1 9 5"
-    checker = CorruptionChecksum.new
     assert_equal 8, checker.checksum(input)
   end
 
   def test_checksum_multiple_lines
     input = "5 1 9 5\n7 5 3\n2 4 6 8"
-    checker = CorruptionChecksum.new
     assert_equal 18, checker.checksum(input)
+  end
+
+  def test_division_single_line
+    input = "5 9 2 8"
+    assert_equal 4, checker.division_checksum(input)
+  end
+
+  def test_division_multiple_lines
+    input = "5 9 2 8\n9 4 7 3\n3 8 6 5"
+    assert_equal 9, checker.division_checksum(input)
   end
 end
