0cc90de2cf27a5be6ada252dea37714b18bc2ab6
David Elbe <david@davidelbe.local>
2017-12-03 12:06:51 +0100

    Add Day 3 - step 2

--- day3/day3.rb
+++ day3/day3.rb
@@ -1,19 +1,46 @@
 require "matrix"
 
 class SpiralMemory
-  def distance_to(input)
-    @number = input.to_i
+  def distance_to(input, from = 1)
+    @number = input.to_i if @number.nil?
     x1, y1 = matrix.index(input)
-    x2, y2 = matrix.index(1)
+    x2, y2 = matrix.index(from)
     (x1 - x2).abs + (y1 - y2).abs
   end
 
-  def matrix
-    @matrix ||= Matrix[*build_matrix(@number)]
+  def value_of(input)
+    @number = input
+    value_array[input - 1]
   end
 
-  def build_matrix(input)
-    n = Math.sqrt(input).ceil
+  def matrix
+    @matrix ||= Matrix[*build_matrix]
+  end
+
+  def value_array
+    @value_array ||= build_value_array
+  end
+
+  # return all neighbouring cells
+  def neighbours(number)
+    x, y = matrix.index(number)
+    [ 
+      matrix[x,   y-1],
+      matrix[x,   y+1],
+      matrix[x-1, y],
+      matrix[x+1, y],
+      matrix[x+1, y+1],
+      matrix[x+1, y-1],
+      matrix[x-1, y-1],
+      matrix[x-1, y+1]
+    ].compact.uniq.delete_if { |cell|
+      x2, y2 = matrix.index(cell)
+      (x2 - x).abs > 1 || (y2 - y).abs > 1 || cell == number
+    }
+  end
+
+  def build_matrix
+    n = Math.sqrt(@number).ceil
     m = Array.new(n){ Array.new(n) }
     position = n * n + 1
     side = n
@@ -25,5 +52,19 @@ class SpiralMemory
       side.downto(1) { |j| m[i+j][i]     = (position -= 1) }
     end
     m
+  end
+
+  def build_value_array
+    # start with an array one 1 and many 0's
+    values = [1] + Array.new(@number, 0)
+    2.upto(@number) do |number|
+      sum = 0
+      neighbours(number).each do |n|
+        sum += values[n - 1].to_i
+        raise "Answer is #{sum}" if sum > @number
+      end
+      values[number - 1] = sum
+    end
+    values
   end
 end
--- day3/day3_test.rb
+++ day3/day3_test.rb
@@ -25,5 +25,28 @@ class SpiralMemoryTest < Minitest::Test
   def test_distance_to_square_265149
     assert_equal 438, spiral.distance_to(265149)
   end
+
+  def test_value_of_square_1
+    assert_equal 1, spiral.value_of(1)
+  end
+
+  def test_value_of_square_2
+    assert_equal 1, spiral.value_of(2)
+  end
+
+  def test_value_of_square_4
+    assert_equal 4, spiral.value_of(4)
+  end
+
+  def test_value_of_square_5
+    assert_equal 5, spiral.value_of(5)
+  end
+
+  # This test won't pass, but the program will raise an
+  # error with the correct answer.
+  def test_value_of_square_265149
+    assert_equal 266330, spiral.value_of(265149)
+  end
+
 end
 
