74eae4856d5df30b51a8f88a5d963127a17b14ce
David Elbe <david@standout.se>
2017-12-07 23:06:48 +0100

    Add day 7 - part 2

--- day7/day7.rb
+++ day7/day7.rb
@@ -28,6 +28,52 @@ class Circus
   end
 
   def bottom_program_name
+    find_bottom_program
     candidate.split(' ').first
+  end
+
+  def total_weight(name)
+    weight_of_stack(name) + weight_of_program(name)
+  end
+
+  def weight_of_program(name)
+    input.scan(/^#{name}\s{1}.(\d{1,})\)/).first.first.to_i
+  end
+
+  def children_of_stack(name)
+    c = input
+        .scan(/^#{name}\s{1}.\d{1,}\).{4}(.{1,})$/)
+    c.empty? ? [] : c[0][0].split(', ')
+  end
+
+  def weight_of_stack(name)
+    children = children_of_stack(name)
+    children
+      .collect { |c| total_weight(c) }
+      .inject(0) { |sum, x| sum + x }
+  end
+
+  def balanced?(stacks)
+    stack_weights = stacks.collect { |stack| total_weight(stack) }
+    stack_weights.uniq.size <= 1
+  end
+
+  def imbalance(stacks)
+    weights = stacks.collect { |stack| total_weight(stack) }
+    pweights = stacks.collect { |name| weight_of_program(name) }
+    @fixed_weight = pweights[weights.index(weights.max)]
+    @fixed_weight -= (weights.max - weights.min)
+    stacks.each do |stack|
+      find_unbalanced(children_of_stack(stack))
+    end
+  end
+
+  def find_unbalanced(stacks)
+    @fixed_weight ||= 0
+    return imbalance(stacks) unless balanced?(stacks)
+    stacks.each do |stack|
+      find_unbalanced(children_of_stack(stack))
+    end
+    @fixed_weight
   end
 end
--- day7/day7_test.rb
+++ day7/day7_test.rb
@@ -4,16 +4,34 @@ require_relative 'day7'
 # Test cases for Circus class
 class CircusTest < Minitest::Test
   def test_bottom_program_example
-    input = File.read('example_input.txt').squeeze("\n")
+    input = File.read('example_input.txt')
     circus = Circus.new(input)
-    circus.find_bottom_program
     assert_equal 'tknk', circus.bottom_program_name
   end
 
   def test_bottom_program_input
-    input = File.read('day7_input.txt').squeeze("\n")
+    input = File.read('day7_input.txt')
     circus = Circus.new(input)
-    circus.find_bottom_program
     assert_equal 'mkxke', circus.bottom_program_name
+  end
+
+  def test_weight_of_example_stacks
+    circus = Circus.new(File.read('example_input.txt'))
+    assert_equal 243, circus.total_weight('fwft')
+    assert_equal 251, circus.total_weight('ugml')
+    assert_equal 243, circus.total_weight('padx')
+    assert_equal 243 + 251 + 243 + 41, circus.total_weight('tknk')
+  end
+
+  def test_unbalanced_example
+    input = File.read('example_input.txt')
+    circus = Circus.new(input)
+    assert_equal 60, circus.find_unbalanced([circus.bottom_program_name])
+  end
+
+  def test_unbalanced
+    input = File.read('day7_input.txt')
+    circus = Circus.new(input)
+    assert_equal 268, circus.find_unbalanced([circus.bottom_program_name])
   end
 end
