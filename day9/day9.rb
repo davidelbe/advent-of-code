b215dc80b28dc0cf0262a8ec309c7d610c034bf7
David Elbe <david@standout.se>
2017-12-09 21:18:47 +0100

    Add Day 9: Stream Processing

---
+++ day9/day9.rb
@@ -0,0 +1,36 @@
+# Day 9: Stream Processing
+class Stream
+  attr_accessor :stream
+
+  def initialize(input)
+    self.stream = input
+    remove_ignored
+  end
+
+  def remove_ignored
+    stream.gsub!(/(!.{1})/, '')
+  end
+
+  def clean!(replace_with = '')
+    stream.gsub!(/<([^>]{0,}>)/, replace_with)
+  end
+
+  def group_count
+    stream.scan(/(\{)/).length
+  end
+
+  def score
+    accumulated_score = 0
+    level = 0
+    while groups.any?
+      level += 1
+      accumulated_score += (level * groups.size)
+      self.stream = groups.collect { |c| c[1..-2] }.join(', ')
+    end
+    accumulated_score
+  end
+
+  def groups
+    stream.scan(/(\{(?>[^}{]+|\g<1>)*\})/).flatten
+  end
+end
---
+++ day9/day9_test.rb
@@ -0,0 +1,53 @@
+require 'minitest/autorun'
+require_relative 'day9'
+
+class StreamTest < Minitest::Test
+  def test_ignored_clean
+    s = Stream.new('{{<!>},{<!>},{<!>},{<a>}}')
+    assert_equal s.stream, '{{<},{<},{<},{<a>}}'
+  end
+
+  def test_garbage_clean
+    s = Stream.new('{{<!>},{<!>},{<!>},{<a>}}')
+    assert_equal s.clean!, '{{}}'
+  end
+
+  def test_group_count_with_ignored_chars
+    s = Stream.new('{{<!>},{<!>},{<!>},{<a>}}')
+    s.clean!
+    assert_equal 2, s.group_count
+  end
+
+  def test_group_count
+    s = Stream.new('{{<a>},{<a>},{<a>},{<a>}}')
+    s.clean!
+    assert_equal 5, s.group_count
+  end
+
+  def test_scores
+    input = ['{}', '{{{}}}', '{{},{}}', '{{{},{},{{}}}}', '{<a>,<a>,<a>,<a>}',
+             '{{<ab>},{<ab>},{<ab>},{<ab>}}', '{{<!!>},{<!!>},{<!!>},{<!!>}}',
+             '{{<a!>},{<a!>},{<a!>},{<ab>}}']
+    expected_scores = [1, 6, 5, 16, 1, 9, 9, 3]
+    input.each_with_index do |i, index|
+      s = Stream.new(i)
+      s.clean!
+      assert_equal expected_scores[index], s.score
+    end
+  end
+
+  def test_real_input_part_1
+    input = File.read('input.txt')
+    s = Stream.new(input)
+    s.clean!
+    assert_equal 11_089, s.score
+  end
+
+  def test_garbage_removal_count
+    s = Stream.new(File.read('input.txt'))
+    before = s.stream.chars.size
+    s.clean!('<>')
+    after = s.stream.chars.size
+    assert_equal 5_288, before - after
+  end
+end
